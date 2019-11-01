551430bde4d5ded6bb857aaf82dc6a647a4ecece
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-11-01 13:08:05 +0100

    Update rando.py

--- rando.py
+++ rando.py
@@ -16,18 +16,18 @@ with TwitchBufferedOutputStream(
 	    twitch_stream_key=args.streamkey,
 	    width=320,
 	    height=240,
-	    fps=15.,
+	    fps=10.,
 	    verbose=True,
 	    enable_audio=True) as videostream:
 
 	frame = np.zeros((240, 320, 3))
 
 	while True:
-	    if videostream.get_video_frame_buffer_state() < 15:
+	    if videostream.get_video_frame_buffer_state() < 10:
 	        frame = np.random.rand(240, 320, 3)
 	        videostream.send_video_frame(frame)
 
-	    if videostream.get_audio_buffer_state() < 15:
+	    if videostream.get_audio_buffer_state() < 10:
 	        left_audio = np.random.randn(2940)
 	        right_audio = np.random.randn(2940)
 	        videostream.send_audio(left_audio, right_audio)
