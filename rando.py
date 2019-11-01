5b61882cedf124294f72e188b9af16e56855dcc7
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-11-01 13:10:39 +0100

    Update rando.py

--- rando.py
+++ rando.py
@@ -3,6 +3,8 @@ from twitchstream.outputvideo import TwitchBufferedOutputStream
 import argparse
 import numpy as np
 
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     required = parser.add_argument_group('required arguments')
@@ -21,13 +23,23 @@ with TwitchBufferedOutputStream(
 	    enable_audio=True) as videostream:
 
 	frame = np.zeros((240, 320, 3))
+	frequency = 100
+        last_phase = 0
 
 	while True:
 	    if videostream.get_video_frame_buffer_state() < 10:
 	        frame = np.random.rand(240, 320, 3)
 	        videostream.send_video_frame(frame)
 
-	    if videostream.get_audio_buffer_state() < 10:
-	        left_audio = np.random.randn(2940)
-	        right_audio = np.random.randn(2940)
-	        videostream.send_audio(left_audio, right_audio)
+	    #if videostream.get_audio_buffer_state() < 10:
+	     #   left_audio = np.random.randn(2940)
+	      #  right_audio = np.random.randn(2940)
+	       # videostream.send_audio(left_audio, right_audio)
+             elif videostream.get_audio_buffer_state() < 30:
+                x = np.linspace(last_phase,
+                                last_phase +
+                                frequency*2*np.pi/videostream.fps,
+                                int(44100 / videostream.fps) + 1)
+                last_phase = x[-1]
+                audio = np.sin(x[:-1])
+                videostream.send_audio(audio, audio)
