fc0cedffe554a41532f00de1a8765aef93c0f1a6
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2021-04-08 21:40:15 +0200

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,1 +1,1 @@
-ffmpeg -re -i ~/twitch/filename.mp4 -vcodec copy -acodec copy -f flv rtmp://live.twitch.tv/app/STREAM_KEY_HERE
+ffmpeg -stream_loop -1 -re -i ~/twitch/filename.mp4 -vcodec copy -acodec copy -f flv rtmp://live.twitch.tv/app/STREAM_KEY_HERE
